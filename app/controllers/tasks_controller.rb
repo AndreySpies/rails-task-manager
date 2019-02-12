18edbb57623badb8c31a61ca234c963eb3c0d4cf
Andrey Spies <andreyspies@gmail.com>
2019-02-12 16:48:36 -0200

    Updated - Clean Code in Task Controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,39 +1,41 @@
 class TasksController < ApplicationController
+  before_action :find, only: %i[show edit destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
   end
 
   def create
-    title = params[:task][:title]
-    details = params[:task][:details]
-    @task = Task.create(title: title, details: details)
+    @task = Task.create(task_params)
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
-    title = params[:task][:title]
-    details = params[:task][:details]
-    completed = params[:task][:completed]
-    @task.update(title: title, details: details, completed: completed)
+    @task = find
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to root_path
+  end
+
+  private
+
+  def find
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
